eb84b426b370f1eb4c8445fadab106d78ef80ddb
Wahn <wahn.unno@gmail.com>
2022-05-03 17:56:37 +0700

    Update Code

---
+++ con_db/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Demo1.con_db
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
