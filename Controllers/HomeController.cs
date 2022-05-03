e7ed9bf8a7acf43eaba545a5b10ddbe6fb628697
Wahn <wahn.unno@gmail.com>
2022-05-03 18:12:26 +0700

    Fix code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,12 +6,14 @@ namespace Demo1.Controllers
 {
     public class HomeController : Controller
     {
+        #region Configuration
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
+        #endregion
 
         #region Call View
         public IActionResult Index()
@@ -28,12 +30,12 @@ namespace Demo1.Controllers
         {
             return View();
         }
-        #endregion
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        #endregion
     }
 }
