c24997e41c296f503b65d1116048684da5ecfd59
Wahn <wahn.unno@gmail.com>
2022-05-09 10:57:15 +0700

    Update Code

--- Models/ManageUserListClass.cs
+++ Models/ManageUserListClass.cs
@@ -2,6 +2,13 @@
 {
     public class ManageUserListClass
     {
+        public string? sOAUserID { get; set; }
+        public string? sName { get; set; }
+        public DateTime? dStartDate { get; set; }
+        public string sStartDate { get; set; }
+        public DateTime? dEndDate { get; set; }
+        public string sEndDate { get; set; }
+        public bool IsDelete { get; set; }
         public List<ManageUserClass>? lstData { get; set; }
     }
 }
