1b2142e0d3be917113b9939c492088174db771cc
Wahn <wahn.unno@gmail.com>
2022-05-18 10:35:49 +0700

    Update code

--- Controllers/ManageUnitDepartmentController.cs
+++ Controllers/ManageUnitDepartmentController.cs
@@ -91,58 +91,40 @@ namespace Demo1.Controllers
 
             try
             {
-                //DateTime? dStartDate = Obj.sStartDate.ToDateFromString("dd/MM/yyyy", "en-US");
-                //DateTime? dEndDate = Obj.sEndDate.ToDateFromString("dd/MM/yyyy", "en-US");
-                //var UPT = DB.UserMaintains.FirstOrDefault(f => f.nID == Obj.nID);
-                //if (UPT != null)
-                //{
-                //    UPT.sOAUserID = Obj.sOAUserID;
-                //    UPT.sRole = Obj.sRole;
-                //    UPT.sName = Obj.sName;
-                //    UPT.sDep = Obj.sDep;
-                //    UPT.sEmail = Obj.sEmail;
-                //    UPT.sTel = Obj.sTel;
-                //    UPT.dStartDate = Obj.dStartDate;
-                //    UPT.dEndDate = Obj.dEndDate;
-                //    UPT.dStartDate = dStartDate.HasValue ? dStartDate.Value : DateTime.Now;
-                //    UPT.dEndDate = dEndDate.HasValue ? dEndDate.Value : DateTime.Now;
-                //    UPT.dUpdateDate = DateTime.Now;
-                //    UPT.sUpdate = null;
-                //    UPT.IsDelete = false;
-                //    DB.SaveChanges();
-                //}
-                //else
-                //{
-                //    var lstDup = DB.UserMaintains.Where(w => w.sOAUserID == Obj.sOAUserID && !w.IsDelete).ToList();
-                //    if (lstDup.Count > 0)
-                //    {
-                //        Result.Message = "OA User is Duplicate.";
-                //        Result.Status = ResultStatus.Failed;
-                //    }
-                //    else
-                //    {
-                //        int nID = DB.UserMaintains.Any() ? DB.UserMaintains.Max(m => m.nID) + 1 : 1;
-
-                //        UserMaintain CRT = new UserMaintain();
-                //        CRT.nID = nID;
-                //        CRT.sOAUserID = Obj.sOAUserID;
-                //        CRT.sRole = Obj.sRole;
-                //        CRT.sName = Obj.sName;
-                //        CRT.sDep = Obj.sDep;
-                //        CRT.sEmail = Obj.sEmail;
-                //        CRT.sTel = Obj.sTel;
-                //        CRT.dStartDate = Obj.dStartDate;
-                //        CRT.dEndDate = Obj.dEndDate;
-                //        CRT.dStartDate = dStartDate.HasValue ? dStartDate.Value : DateTime.Now;
-                //        CRT.dEndDate = dEndDate.HasValue ? dEndDate.Value : DateTime.Now;
-                //        CRT.dCreateDate = DateTime.Now;
-                //        CRT.sCreate = null;
-                //        CRT.IsDelete = false;
-                //        DB.UserMaintains.Add(CRT);
-                //        DB.SaveChanges();
-                //    }
-                //}
-                //Result.Status = ResultStatus.Success;
+                var UPT = DB.Sections.FirstOrDefault(f => f.SectionCode == Obj.SectionCode);
+                if (UPT != null)
+                {
+                    UPT.SectionName = Obj.SectionName;
+                    UPT.SectionGroup = Obj.SectionGroup;
+                    UPT.dUpdateDate = DateTime.Now;
+                    UPT.sUpdate = null;
+                    UPT.IsDelete = false;
+                    DB.SaveChanges();
+                }
+                else
+                {
+                    var lstDup = DB.Sections.Where(w => w.SectionCode == Obj.SectionCode && !w.IsDelete).ToList();
+                    if (lstDup.Count > 0)
+                    {
+                        Result.Message = "Division Code is Duplicate.";
+                        Result.Status = ResultStatus.Failed;
+                    }
+                    else
+                    {
+                        Section CRT = new Section();
+                        CRT.SectionCode = Obj.SectionCode;
+                        CRT.SectionName = Obj.SectionName;
+                        CRT.SectionGroup = Obj.SectionGroup;
+                        CRT.dCreateDate = DateTime.Now;
+                        CRT.sCreate = null;
+                        CRT.dUpdateDate = DateTime.Now;
+                        CRT.sUpdate = null;
+                        CRT.IsDelete = false;
+                        DB.Sections.Add(CRT);
+                        DB.SaveChanges();
+                    }
+                }
+                Result.Status = ResultStatus.Success;
             }
             catch (Exception ex)
             {
@@ -154,20 +136,21 @@ namespace Demo1.Controllers
         }
 
         [HttpPost]
-        public ResultAPI Delete(int nID = 0)
+        public ResultAPI Delete(string sID)
         {
             ResultAPI Result = new ResultAPI();
+
             try
             {
-                var DEL = DB.UserMaintains.FirstOrDefault(f => f.nID == nID);
+                var DEL = DB.Sections.FirstOrDefault(f => f.SectionCode == sID);
                 if (DEL != null)
                 {
                     DEL.dUpdateDate = DateTime.Now;
                     DEL.sUpdate = null;
                     DEL.IsDelete = true;
                     DB.SaveChanges();
+                    Result.Status = ResultStatus.Success;
                 }
-                Result.Status = ResultStatus.Success;
             }
             catch (Exception ex)
             {
